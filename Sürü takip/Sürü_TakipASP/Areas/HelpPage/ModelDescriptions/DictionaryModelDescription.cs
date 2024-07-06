656a244a1cecf8c759c0c223c86aa155304b5122
gokselgokcen <114031280+gokselgokcen@users.noreply.github.com>
2024-07-06 17:36:48 +0300

    SuruTakip

---
+++ Sürü takip/Sürü_TakipASP/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Sürü_Takip.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Sürü takip/Sürü_TakipASP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sürü_Takip.WebApiApplication" Language="C#" %>
