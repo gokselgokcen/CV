36d8bc1244080c6d62056d993b1623c37e9d54f8
gokselgokcen <114031280+gokselgokcen@users.noreply.github.com>
2024-07-12 12:41:30 +0300

    SürüTakipUpd

---
+++ SürüTakip/Breeding.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace SürüTakip
+{
+    partial class Breeding
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dgv1 = new System.Windows.Forms.DataGridView();
+            this.lblID = new System.Windows.Forms.Label();
+            this.lblNo = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.lblCheck = new System.Windows.Forms.Label();
+            this.lblKuru = new System.Windows.Forms.Label();
+            this.dgv2 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(161, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(51, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Kulak Numarası:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(142, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "İsmi:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(24, 184);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(165, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Tohumlanma Tarihi:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(69, 242);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(120, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Kontrol Tarihi:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(10, 301);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(179, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Kuruya ayrılma Tarihi:";
+            // 
+            // dgv1
+            // 
+            this.dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv1.Location = new System.Drawing.Point(466, 50);
+            this.dgv1.Name = "dgv1";
+            this.dgv1.Size = new System.Drawing.Size(346, 561);
+            this.dgv1.TabIndex = 6;
+            this.dgv1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv1_CellClick_1);
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblID.Location = new System.Drawing.Point(231, 32);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(0, 18);
+            this.lblID.TabIndex = 7;
+            this.lblID.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // lblNo
+            // 
+            this.lblNo.AutoSize = true;
+            this.lblNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblNo.Location = new System.Drawing.Point(231, 77);
+            this.lblNo.Name = "lblNo";
+            this.lblNo.Size = new System.Drawing.Size(0, 18);
+            this.lblNo.TabIndex = 8;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblName.Location = new System.Drawing.Point(231, 137);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(0, 18);
+            this.lblName.TabIndex = 9;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(234, 184);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(226, 24);
+            this.dateTimePicker1.TabIndex = 10;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // lblCheck
+            // 
+            this.lblCheck.AutoSize = true;
+            this.lblCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblCheck.Location = new System.Drawing.Point(231, 243);
+            this.lblCheck.Name = "lblCheck";
+            this.lblCheck.Size = new System.Drawing.Size(0, 18);
+            this.lblCheck.TabIndex = 11;
+            // 
+            // lblKuru
+            // 
+            this.lblKuru.AutoSize = true;
+            this.lblKuru.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblKuru.Location = new System.Drawing.Point(231, 303);
+            this.lblKuru.Name = "lblKuru";
+            this.lblKuru.Size = new System.Drawing.Size(0, 18);
+            this.lblKuru.TabIndex = 12;
+            // 
+            // dgv2
+            // 
+            this.dgv2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv2.Location = new System.Drawing.Point(844, 50);
+            this.dgv2.Name = "dgv2";
+            this.dgv2.Size = new System.Drawing.Size(557, 561);
+            this.dgv2.TabIndex = 13;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(234, 363);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 33);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Kaydet";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Breeding
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1413, 623);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dgv2);
+            this.Controls.Add(this.lblKuru);
+            this.Controls.Add(this.lblCheck);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblNo);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.dgv1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Breeding";
+            this.Text = "Tohumlama Giriş";
+            this.Load += new System.EventHandler(this.Breeding_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView dgv1;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.Label lblNo;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label lblCheck;
+        private System.Windows.Forms.Label lblKuru;
+        private System.Windows.Forms.DataGridView dgv2;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ SürüTakip/Breeding.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SürüTakip
+{
+    public partial class Breeding : Form
+    {
+        public Breeding()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void LoadData()
+        {
+            string connectionString = @"Data Source=DESKTOP-NA96GMK\GG_MSSQLSERVER1;Initial Catalog=NG_surutakip;Integrated Security=True;Connect Timeout=30;Encrypt=True;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+
+            SqlDataAdapter da = new SqlDataAdapter("SELECT  CattleID,EarTagNumber,Name  FROM dbo.CATTLE WHERE Gender = 'DİŞİ' ", conn);
+            dgv1.Columns.Clear();
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dgv1.DataSource = dt;
+
+            dgv1.Columns[0].HeaderText = "ID";
+            dgv1.Columns[1].HeaderText = "Kulak No";
+            dgv1.Columns[2].HeaderText = "Adı";
+            conn.Close();
+        }
+
+        private void Breeding_Load(object sender, EventArgs e)
+        {
+            LoadData();
+            LoadBreedData();
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void dgv1_CellClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            int rowIndex = e.RowIndex;
+
+
+            if (rowIndex >= 0)
+            {
+                var IDValue = dgv1.Rows[rowIndex].Cells[0].Value.ToString();
+                var numberValue = dgv1.Rows[rowIndex].Cells[1].Value.ToString();
+                var nameValue = dgv1.Rows[rowIndex].Cells[2].Value.ToString();
+
+                lblNo.Text = numberValue;
+                lblName.Text = nameValue;
+                lblID.Text = IDValue;
+
+
+            }
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            DateTime selectedDate = dateTimePicker1.Value;
+            DateTime checkDate = selectedDate.AddDays(60);
+            lblCheck.Text = checkDate.ToString("dd.MM.yyyy");
+
+            DateTime Sd = dateTimePicker1.Value;
+            DateTime KuruDate = Sd.AddDays(210);
+            lblKuru.Text = KuruDate.ToString("dd.MM.yyyy");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connectionString = @"Data Source=DESKTOP-NA96GMK\GG_MSSQLSERVER1;Initial Catalog=NG_surutakip;Integrated Security=True;Connect Timeout=30;Encrypt=True;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("INSERT INTO dbo.Breed (CattleID,EarTagNumber,Name,BreedDate,CheckDate,KuruDate) " +
+               "VALUES (@id,@kulak,@name,@breed,@check,@kuru) ", conn);
+
+            
+            if(string.IsNullOrWhiteSpace(lblCheck.Text))
+            {
+                MessageBox.Show("Tarih Seçimi Yapınız");
+
+            }
+            else
+            {
+                cmd.Parameters.AddWithValue("@id", lblID.Text);
+                cmd.Parameters.AddWithValue("@kulak", lblNo.Text);
+                cmd.Parameters.AddWithValue("@name", lblName.Text);
+                cmd.Parameters.AddWithValue("@breed", dateTimePicker1.Value);
+                cmd.Parameters.AddWithValue("@check", DateTime.Parse(lblCheck.Text));
+                cmd.Parameters.AddWithValue("@kuru", DateTime.Parse(lblKuru.Text));
+                cmd.ExecuteNonQuery();
+                LoadBreedData();
+
+            }
+
+//cmd.ExecuteNonQuery();
+
+            conn.Close();
+            //LoadBreedData();
+            
+
+
+
+        }
+
+        private void LoadBreedData()
+        {
+            string connectionString = @"Data Source=DESKTOP-NA96GMK\GG_MSSQLSERVER1;Initial Catalog=NG_surutakip;Integrated Security=True;Connect Timeout=30;Encrypt=True;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+
+            SqlDataAdapter da = new SqlDataAdapter("SELECT EarTagNumber,Name,BreedDate,CheckDate,KuruDate  FROM dbo.Breed", conn);
+            //dgv1.Columns.Clear();
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dgv2.DataSource = dt;
+
+            dgv2.Columns[0].HeaderText = "Kulak No";
+            dgv2.Columns[1].HeaderText = "İsim";
+            dgv2.Columns[2].HeaderText = "Tohumlama";
+            dgv2.Columns[3].HeaderText = "Kontrol";
+            dgv2.Columns[4].HeaderText = "Kuru";
+
+            conn.Close();
+            //LoadData();
+        }
+    }
+}
+
+
